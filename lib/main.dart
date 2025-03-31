51607746ccb5cab09736cde2f4ad455aa743a1c2
Marilise Morona <marymorona@gmail.com>
2025-03-31 15:59:48 -0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+
+void main() {
+  runApp(const MainApp());
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: Scaffold(
+          floatingActionButton: BackButton(),
+          backgroundColor: Color.fromARGB(123, 7, 218, 255),
+            body: Center(
+              child: Text('Testando run flutter'),
+          )
+        ),
+      );
+  }
+}
